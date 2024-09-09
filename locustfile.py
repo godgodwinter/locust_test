ad46c59ca459d3134b6611ef1e8a0d4e4efde6fc
godgodwinter <kakadz.pro@gmail.com>
2024-09-09 11:25:26 +0700

    update

--- locustfile.py
+++ locustfile.py
@@ -1,16 +1,22 @@
 from locust import HttpUser, task
 
 class GetDataAPI(HttpUser):
+    # jwt_token = "YOUR_JWT_TOKEN_HERE"
+    jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtYSI6IlJJU0tBIEFVTElBIE5BWkFSSU5BIiwicm9sZSI6ImMybHpkMkU9IiwiaWF0IjoxNzI1ODU1NzQ3LCJleHAiOjE3MjY0NjA1NDd9.kEvHdtP7QhbNsEkX6OBpsP2ZXjyNC-TCP57_wfWC3fI"
     @task
     def hello_world(self):
+        headers = {
+            "Authorization": f"Bearer {self.jwt_token}"
+        }
+        # self.client.get("/api/v1/admin/sekolah/index", headers=headers)
+        self.client.get("/redis/UJIANKHUSUS_V3_PROSES_SISWAID_B_1", headers=headers)
+        # self.client.get("/redis/UJIANKHUSUS_V3_PROSES_SISWAID_A_1", headers=headers)
+        # self.client.get("/api/v2/master/sekolah")
         # self.client.get("/")
         # self.client.get("/profile")
-        # self.client.get("/redis/UJIANKHUSUS_PROSES_SISWA_ID_21597")
-        self.client.get("/redis/UJIANKHUSUS_V3_PROSES_SISWAID_B_21597/aspek/5")
-        # self.client.get("/redis/UJIANKHUSUS_V3_PROSES_SISWAID_A_21597/aspek/5")
-        # self.client.get("/redis/UJIANKHUSUS_V3_PROSES_SISWAID_A_21597/full_soal/5")
+        # self.client.get("/redis/UJIANKHUSUS_PROSES_SISWA_ID_18593")
         # self.client.get("/sekolah/68/kelas/989")
-        # self.client.get("/api/v1/ujiankhusus/proses/siswa/21597")
+        # self.client.get("/api/v1/ujiankhusus/proses/siswa/17874")
         # self.client.get("/api/v1/ujiankhusus/proses")
         # self.client.get("/hello")
         # self.client.get("/world")
