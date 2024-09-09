7ed6cfc163b99bba34c8045a6d65601ea6a39f89
godgodwinter <kakadz.pro@gmail.com>
2024-09-09 12:55:21 +0700

    update

--- locustfile.py
+++ locustfile.py
@@ -4,12 +4,33 @@ class GetDataAPI(HttpUser):
     # jwt_token = "YOUR_JWT_TOKEN_HERE"
     jwt_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtYSI6IlJJU0tBIEFVTElBIE5BWkFSSU5BIiwicm9sZSI6ImMybHpkMkU9IiwiaWF0IjoxNzI1ODU1NzQ3LCJleHAiOjE3MjY0NjA1NDd9.kEvHdtP7QhbNsEkX6OBpsP2ZXjyNC-TCP57_wfWC3fI"
     @task
-    def hello_world(self):
+    def send_answer(self):
         headers = {
-            "Authorization": f"Bearer {self.jwt_token}"
+            "Authorization": f"Bearer {self.jwt_token}",
+            "Content-Type": "application/json"
         }
+        payload = {
+            "kode_soal": "65b898b5390d668bfb7322df",
+            "index_soal": 0,
+            "kode_jawaban": "ed45251f-ba81-440a-b479-3ddbcaa6f1a4"
+        }
+        self.client.post("/api/v1/client_siswa/ujiankhusus/paketsoal/detail/65a44ab921be5acd2d3da797/jawab", json=payload, headers=headers)
+    # @task
+    # def hello_world(self):
+    #     headers = {
+    #         "Authorization": f"Bearer {self.jwt_token}"
+    #     }
+        # !getall soal kn
+        #self.client.get("/api/v1/client_siswa/ujiankhusus/paketsoal/detail/65a44ab921be5acd2d3da797", headers=headers)
+        # !skrip do jawab simpan
+    #     self.client.get("/api/v1/client_siswa/ujiankhusus/paketsoal/detail/65a44ab921be5acd2d3da797/jawab", headers=headers)
+        
+       
+
+
+
         # self.client.get("/api/v1/admin/sekolah/index", headers=headers)
-        self.client.get("/redis/UJIANKHUSUS_V3_PROSES_SISWAID_B_1", headers=headers)
+        # self.client.get("/redis/UJIANKHUSUS_V3_PROSES_SISWAID_B_1", headers=headers)
         # self.client.get("/redis/UJIANKHUSUS_V3_PROSES_SISWAID_A_1", headers=headers)
         # self.client.get("/api/v2/master/sekolah")
         # self.client.get("/")
